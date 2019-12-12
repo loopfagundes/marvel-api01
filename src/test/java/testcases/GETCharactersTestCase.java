b7b0b9bc179b180ce41c2d381e93776d158bb8c0
luciano.fagundes <luciano.fagundes@dbserver.com.br>
2019-12-12 12:01:42 -0200

    update api test/tentativa arrumar testcase, testng e utils (nao funcionou)

---
+++ src/test/java/testcases/GETCharactersTestCase.java
@@ -0,0 +1,36 @@
+package testcases;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.sql.Timestamp;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static utils.HashUtil.getHash;
+
+public class GETCharactersTestCase {
+
+    @Parameters({"apiKey", "privateApiKey", "baseUrl", "characterEP"})
+    @Test
+    public void getCharactersWithSuccess(String apiKey, String privateApiKey, String baseUrl, String characterEP) {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        String timesTampStr = timestamp.toString();
+
+        String md5Str = timesTampStr + privateApiKey + apiKey;
+
+        String hash = getHash(md5Str);
+
+                given().
+                        baseUri("https://gateway.marvel.com/v1/public").
+                        basePath("characters").
+                        param("ts", timesTampStr).
+                        param("apiKey",apiKey).
+                        param("hash",hash).
+                        param("name","Thor").
+                when().
+                        get().
+                then().
+                        assertThat().body(matchesJsonSchemaInClasspath("GET_v1_public_characters_schema.json"));
+    }
+}
